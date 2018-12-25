607979f9329ac5abe03129e006926144814c9ad7
baukevw <bauke@baukevanwieren.nl>
2018-12-25 15:15:42 +0100

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+  get 'pages/index'
+  mount Sidekiq::Web => '/sidekiq'
+  devise_for :users
+  root to: 'pages#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
