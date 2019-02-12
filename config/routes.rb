c3c4a52d9515578f3773a11dd8618ebd4e36c5a5
baukevw <bauke@baukevanwieren.nl>
2019-02-12 11:29:00 +0100

    sidekiq only available for logged in admin users Bugfix for iCheck boxes

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
     resources :areas
   end
 
-  mount Sidekiq::Web => '/sidekiq'
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
