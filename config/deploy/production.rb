581be5a00d564e519abd2fe08af767e3c85829ef
baukevw <bauke@baukevanwieren.nl>
2019-03-03 17:26:05 +0100

    deploy from thermostat branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ server '213.10.240.190:22', user: 'deploy', roles: %w{web app db}
 set :deploy_to, '/var/www/b_home/production'
 
 set :rails_env, 'production'
-set :branch, 'develop'
+set :branch, 'feature/thermostat_device'
